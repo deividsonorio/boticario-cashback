7de3a4d656b408ad27fa6e792d257f30dc0c7256
Deivid S. Onorio <deividsonorio@gmail.com>
2022-08-01 20:01:54 -0300

    Create super user for dev purposes

--- docker/app/start-dev-server.sh
+++ docker/app/start-dev-server.sh
@@ -2,5 +2,6 @@
 echo "Rodando migrations..."
 python manage.py makemigrations
 python manage.py migrate
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('08360313938', 'teste@boticario.com', 'teste123', cpf='08360313938', first_name='Teste', last_name='Boticario')"
 echo "Iniciando servidor..."
 python manage.py runserver 0.0.0.0:8000
